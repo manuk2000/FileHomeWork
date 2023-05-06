f6033842d0121fd9570c8a237f7d9db84823bbf4
manuk2000 <asatryanmanuk2000@gmail.com>
2023-05-06 10:12:55 +0400

    dfdf

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Q q = new QQ();
+        q.foo1();
+    }
+}
+class Q{
+    static void foo1(){
+
+    }
+}
+class QQ extends Q{
+
+}
+
+
+
+
+
